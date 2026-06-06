a0efcee3b7db9d1a631a5bf6e6b5657a99bb21fa
Michael Polom <michaelpolom@gmail.com>
2026-06-06 17:32:09 +0100

    feat: add contact form and email handling functionality

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,9 +6,9 @@ import { legacyImgSize, imgSize, obsidianImgSize } from "@mdit/plugin-img-size";
 import { RenderPlugin } from "@11ty/eleventy";
 
 export default async function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("src/css/core.style.css");
+	eleventyConfig.addPassthroughCopy("./src/css/core.style.css");
 	eleventyConfig.addPassthroughCopy({'./node_modules/@fontsource-variable/geist/files/*.woff2': 'css/files'})
-	eleventyConfig.addPassthroughCopy("src/images");
+	eleventyConfig.addPassthroughCopy("./src/images");
 	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(imgSize));
 	eleventyConfig.addPlugin(eleventySass);
 	eleventyConfig.addPlugin(RenderPlugin);
